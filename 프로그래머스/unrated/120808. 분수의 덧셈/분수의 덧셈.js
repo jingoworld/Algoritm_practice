d5aecf9c5d9135cd4da914b701f344c397e9d25a
Jinwoo Choi <lonul83@gmail.com>
2022-10-09 00:23:02 +0900

    [unrated] Title: 분수의 덧셈, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/unrated/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,11 +1,7 @@
 function solution(denum1, num1, denum2, num2) {
-    // let A = [denum1 * num2, num1 * num2]
-    // let B = [denum2 * num1, num1 * num2]
-    let num = (denum1 * num2) + (denum2 * num1)
-    let den = (num1 * num2)
-    // let result = [num, den] // 약분이 안된 상태
-    // 유클리드 호제법으로 최소공약수 찾기
-    const gsm = (num, den) => {
+    let num = (denum1 * num2) + (denum2 * num1)  // 분자
+    let den = (num1 * num2)                      // 분모
+    const gsm = (num, den) => {                  // 최대공약수
         if (den === 0) return num;
         return gsm(den, num % den);
     }
