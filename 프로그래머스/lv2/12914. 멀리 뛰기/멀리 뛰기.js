d5c39b9ca505c1ed0a0e5166ea885d775aa027ff
Jinwoo Choi <lonul83@gmail.com>
2022-10-24 18:41:03 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    if (n === 1 || n === 2) return n;
+    const arr = [1, 2];
+    for (let i = 2; i < n; i++) {
+        arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+    }
+    return arr.pop()
+}
